e15747b6d8ddf30ae419facbc670aa861369a500
Denis Vakulenko <woof88@yandex.ru>
2022-12-03 15:49:48 +0500

    feat: cli:args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,40 +1,16 @@
-// const parseArgs = () => {
-//   const proc = process.argv.slice(2);
-
-//     // for( let i = 0; i < proc.length; i++) {
-//     //   if(i.startsWith('--')) {
-//     //     console.log(`${i} is ${i++}`)
-//     //   }
-
-//   let arr = []
-//   proc.forEach((item) => {
-//     if(item.startsWith('--')) {
-//       arr.push(item)
-//     }
-//   })  
-
-//   console.log(proc)
-// };
-
-// parseArgs();
-
 const parseArgs = () => {
   const proc = process.argv.slice(2);
 
-  let arr = [];
-  let arr2 = [];
-
-  proc.forEach((item, i) => {
-    if((i % 2 === 0)) {
-      arr.push(item);
-    } else {
-      arr2.push(item)
+  const res = proc.reduce((acc, value, i, arr) => {
+    if (value.startsWith("--")) {
+      return acc + value + " is ";
+    } else if (arr.length === i + 1) {
+      return acc + value;
     }
-  })
+    return acc + value + ", ";
+  }, "");
 
-  console.log(arr)
-  console.log(arr2)
-  console.log(proc)
+  console.log(res);
 };
 
 parseArgs();
--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,28 @@
-const read = async () => {
-    // Write your code here 
-};
+// import { dirname } from "node:path";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// const readable = getReadableStreamSomehow();
 
-await read();
+// const read = async () => {
+//   const __filename = fileURLToPath(import.meta.url);
+//   const __dirname = dirname(__filename);
+//   const dirPath = path.join(__dirname, "./files");
+//   const copyPath = path.join(__dirname, "/files_copy");
+
+//   // 'readable' may be triggered multiple times as data is buffered in
+//   readable.on("readable", () => {
+//     let chunk;
+//     console.log("Stream is readable (new data received in buffer)");
+//     // Use a loop to make sure we read all currently available data
+//     while (null !== (chunk = readable.read())) {
+//       console.log(`Read ${chunk.length} bytes of data...`);
+//     }
+//   });
+
+//   // 'end' will be triggered once when there is no more data available
+//   readable.on("end", () => {
+//     console.log("Reached end of stream.");
+//   });
+// };
+
+// await read();
